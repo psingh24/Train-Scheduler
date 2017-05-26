b1eafef8fd5f056b84bc36af99a0dbcff4469d9d
Prabhdeep <ps591990@gmail.com>
2017-05-26 00:06:29 -0500

    update

--- assets/app.js
+++ assets/app.js
@@ -11,123 +11,80 @@ $(document).ready(function() {
   firebase.initializeApp(config);
 
 var database = firebase.database();
-
-// console.log(firebase.database.ServerValue.TIMESTAMP)
-
-var ref = database.ref();
-
-// ref.on('value', getData)
-
-// console.log(database)
-
-var today = moment().format();
-// console.log(today)
-
-
-
-// var today = moment(new Date()); //todays date
-// var end = moment("2015-09-02"); // another date
-// var duration = moment.duration(today.diff(end));
-var duration;
-// console.log(months)
-
-
-// var data = snapshot.val()
-//     console.log("The start date is: " + data.startDate)
-//     var start = moment(data.startDate, "MM/DD/YYYY");
-//     console.log("Start is: " + start)
-//     var durationS = moment.durationS(today.diff(start));
-
-
-
-
+var ref = database.ref("train");
+var trainName; 
+var destination; 
+var firstTrain;
+var frequency; 
+var firstTimeConverted; 
+var diffTime; 
+var Remainder; 
+var minTillNextTrain; 
+var nextTrain; 
+var nextTrainFormatted; 
 
 
 ref.on("child_added", function(snapshot) {
-
-
     var data = snapshot.val()
-// console.log(fristTrain)
-    var dateDiff = 0
-    var convertedDate = moment().hour(data.fristTrain);
-    // console.log(convertedDate)
-    dateDiff = data.fristTrain + data.frequency
-
-    // var now = moment().format("HH:mm A")
-    // console.log(now)
-
-    var next = data.fristTrain.add(data.frequency, "m")
-    console.log(next)
-    // console.log(dateDiff)
-    // console.log("Today is: " + moment().format() + " start date is: " + convertedDate + " Date dif is: " + dateDiff);
-    // var totalRate = dateDiff * data.frequency
-
- 
- $("table").append("<tr><td>"+data.trainName+"</td><td>"+data.destination+"</td><td>"+data.frequency+"</td><td>"+dateDiff +"</td><td>"+data.fristTrain +"</td></tr>")
-
-  
-  // $("table").append("<tr><td>"+data.employeeName+"</td><td>"+data.role+"</td><td>"+data.startDate+"</td><td>"+duration+"</td><td>"+data.monthlyRate +"</td><td>Germany</td></tr>")
-
-
-  // console.log( data.employeeName, data.role , data.startDate,  );
+    console.log(data)
+    console.log(Object.key)
+    
+    var key = ref.key
+ console.log(key)
+        $("table").append("<tr class='train' id='hello'><td>"+data.trainName+"</td><td>"+data.destination+"</td><td>"+data.frequency+"</td><td>"+data.nextArrival +"</td><td>"+data.nextTrain +"</td><td><button class='removeTrain btn btn-info'>Remove Train</button></td></tr>")
 });
-
-
-
-
-
-
-
-
-
 
 $("#submit").on("click", function(event) {
 
-event.preventDefault();
+    event.preventDefault();
 
-// var mom = moment('171054', 'HHmmss');
-// console.log(mom.format());
-// console.log(mom.format('HH:mm:ss'));
-
-
-
-var trainName = $("#trainName").val().trim();
-var destination = $("#destination").val().trim()
-var fristTrain = $("#fristTrain").val().trim();
-// console.log(startDate)
-var trainTime = moment(fristTrain, "HH:mm A")
-var trainPretty = moment(trainTime).format("HH:mm A")
-
-var nextArrival = moment(trainTime).diff(moment(), "minutes")
-// var convertedDate = moment(new Date(startDate));
-// startDate.format("DD/MM/YYYY")
-var frequency =  $("#frequency").val().trim()
+    trainName = $("#trainName").val().trim();
+    destination = $("#destination").val().trim()
+    firstTrain = $("#fristTrain").val().trim();
+    frequency =  parseInt($("#frequency").val().trim())
 
 
-database.ref().push( {
-    trainName: trainName,
-    destination: destination,
-    fristTrain: trainPretty,
-    frequency: frequency,
-    // monthsWorked: duration
-    // totalBilled: 
+    firstTimeConverted = moment(firstTrain, "hh:mm");
+    diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    Remainder = diffTime % frequency;
+    minTillNextTrain = frequency - Remainder;
+    nextTrain = moment().add(minTillNextTrain, "minutes");
+    nextTrainFormatted = moment(nextTrain).format("hh:mm A");
+          
 
-})
+        database.ref("train").push( {
+            trainName: trainName,
+            destination: destination,
+            fristTrain: firstTrain,
+            frequency: frequency,
+            nextTrain: minTillNextTrain,
+            nextArrival: nextTrainFormatted
+
+        })
+
+       $("#trainName").val('')
+        $("#destination").val('')
+        $("#fristTrain").val('')
+        $("#frequency").val('')
+
+    })
+
+function removeTrain() {
+    
+     $(this).closest('tr').remove();
+     getKey = $(this).parent().parent().attr("id");
+     console.log(getKey)
+     ref.child(getKey).remove();
+
+}
+
+
+$(document).on("click", ".removeTrain", removeTrain)
 
 
 
-
-
-})
-
-
-
-// console.log(moment().format())
-
-
-// console.log(moment(1318781876));
-
-
-
-
+function signIn() {
+    $("#main").hide()
+}     
+signIn()
 })
